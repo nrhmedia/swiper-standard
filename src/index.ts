661de9e068f35a02f91c40605f4643628de56a22
nrhmedia <git@nrhmedia.com>
2024-09-24 16:53:27 -0700

    01

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
-import { greetUser } from '$utils/greet';
+document.querySelectorAll('.swiper.is-default').forEach((swiperElement) => {
+  const rewindAttribute = swiperElement.getAttribute('swiper-rewind');
+  const shouldRewind = rewindAttribute === 'true';
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const swiper = new Swiper(swiperElement, {
+    speed: 400,
+    navigation: {
+      nextEl: swiperElement.querySelector(
+        '.swiper-navigation.is-default .swiper-navigation-button.is-next'
+      ),
+      prevEl: swiperElement.querySelector(
+        '.swiper-navigation.is-default .swiper-navigation-button.is-prev'
+      ),
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    rewind: shouldRewind,
+    spaceBetween: 16, // mobile, default
+    slidesPerView: 1, // mobile, default
+    breakpoints: {
+      992: {
+        // desktop
+        slidesPerView: 4,
+        spaceBetween: 16,
+      },
+      768: {
+        // tablet
+        slidesPerView: 3,
+        spaceBetween: 16,
+      },
+      480: {
+        // mobile landscape
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+    },
+  });
 });
